3187e9a0a9fd77301410258ebb4f14a875215f02
soares-joao <soares.vitorss@icloud.com>
2024-12-13 21:18:16 -0300

    Add files via upload

---
+++ site/db.php
@@ -0,0 +1,18 @@
+
+<?php
+$host = 'localhost';
+$user = 'root'; // Usuário padrão do XAMPP
+$pass = ''; // Senha padrão 
+$db = 'crud_tarefas';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+else{
+    echo "lacrou";
+}
+?>
+
